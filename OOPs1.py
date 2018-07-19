1f658414fbf5abf5c49a405d7b28c2162c99736c
agarwalanant <agarwal_anant@outlook.com>
2018-07-19 11:30:13 +0530

    "v0.001"

--- OOPs1.py
+++ OOPs1.py
@@ -6,11 +6,13 @@ class Account:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            print("Aomunt deposited")
             self.show_balance()
 
     def withdraw(self, amount):
         if 0 < amount < self.balance:
             self.balance -= amount
+            print("Amount Withdrawn")
         else:
             print("insufficient balance")
         self.show_balance()
@@ -19,3 +21,8 @@ class Account:
         print("Balance is {}".format(self.balance))
 
 
+Anant = Account("Anant", 1000)
+
+Anant.deposit(1000)
+Anant.withdraw(200)
+Anant.withdraw(5000)
