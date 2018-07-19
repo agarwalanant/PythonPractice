fa717dc685d7d722478688345bbde26468eeb473
agarwalanant <agarwal_anant@outlook.com>
2018-07-19 12:59:43 +0530

    "Doc String added"

--- OOPs1.py
+++ OOPs1.py
@@ -1,24 +1,28 @@
+
+
+
 class Account:
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
+        self._name = name
+        self.__balance = balance
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             print("Aomunt deposited")
             self.show_balance()
 
     def withdraw(self, amount):
-        if 0 < amount < self.balance:
-            self.balance -= amount
+        if 0 < amount < self.__balance:
+            self.__balance -= amount
             print("Amount Withdrawn")
         else:
             print("insufficient balance")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
 
 Anant = Account("Anant", 1000)
@@ -26,3 +30,4 @@ Anant = Account("Anant", 1000)
 Anant.deposit(1000)
 Anant.withdraw(200)
 Anant.withdraw(5000)
+
